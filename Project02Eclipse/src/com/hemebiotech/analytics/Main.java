6d853293cea0107e203302aa33096c82de85057a
pierre-edouard iung <contact@pierre-edouard.tech>
2024-07-17 16:14:32 +0200

    chore: add javadoc comments

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -2,7 +2,10 @@ package com.hemebiotech.analytics;
 
 import com.hemebiotech.symptommanager.SymptomManager;
 import com.hemebiotech.symptommanager.SymptomManagerImpl;
-
+/**
+ * This program will take a list of symptoms as inputs
+ * and return a sorted and counted list of symptoms as output (without symptoms duplicated)
+ */
 public class Main {
 
 	public static void main(String args[]) throws Exception {
--- Project02Eclipse/src/com/hemebiotech/symptomcounter/CountSymptomsFromList.java
+++ Project02Eclipse/src/com/hemebiotech/symptomcounter/CountSymptomsFromList.java
@@ -6,6 +6,13 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class CountSymptomsFromList implements SymptomCounter {
+    /**
+     * <p> Count the symptoms given as inputs of our program
+     * @param symptoms the list of input symptoms
+     * @return a Map of counted (but unsorted) <symptoms,count>
+     * this Map will not be sorted alphabetically yet
+     */
+
     @Override
     public Map<String, Integer> countSymptoms(List<String> symptoms) {
 
@@ -23,8 +30,15 @@ public class CountSymptomsFromList implements SymptomCounter {
 
         return result;
     }
-
-    // Creating a TreeMap, which will automatically sort alphabetically the entries by key, as my key are String
+    /**
+     * <p> Sort the symptoms by using a TreeMap as output
+     * @param unsortedSymptoms the Map<key,value> of counted symptoms
+     *                         where key = a given symptom,
+     *                         value = counter for the given symptom
+     * @return a TreeMap of sorted <symptoms,count>
+     * this TreeMap will automatically sort alphabetically the entries by key, as my key are String
+     */
+    // Creating a TreeMap,
     public Map<String, Integer> sortSymptoms(Map<String, Integer> unsortedSymptoms){
 
         return new TreeMap<>(unsortedSymptoms);
