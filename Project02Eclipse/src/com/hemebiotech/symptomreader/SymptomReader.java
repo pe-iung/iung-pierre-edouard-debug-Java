7ee129298c15ba8c9bb233c74727c91e1a174748
pierre-edouard iung <contact@pierre-edouard.tech>
2024-07-29 19:57:51 +0200

    Updating .gitignore

--- Project02Eclipse/src/com/hemebiotech/symptomreader/SymptomReader.java
+++
@@ -1,7 +0,0 @@
-package com.hemebiotech.symptomreader;
-
-import java.util.List;
-
-public interface SymptomReader {
-    List<String> getSymptoms() throws Exception;
-}
--- Project02Eclipse/src/com/hemebiotech/symptomwriter/SymptomWriter.java
+++
@@ -1,8 +0,0 @@
-package com.hemebiotech.symptomwriter;
-
-import java.io.IOException;
-import java.util.Map;
-
-public interface SymptomWriter {
-    void writeSymptoms(Map<String, Integer> symptoms) throws IOException;
-}
